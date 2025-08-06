34bcd5fb191b6cf5f8fc06e88fa2a29fe5166828
SM0KE <a1b3h8t5@gmail.com>
2025-08-07 01:59:12 +0330

    update

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
-export default defineConfig({
-  base: './',
-=======
 
 export default defineConfig({
-  base: './', 
->>>>>>> f20846ca6df45af9f0dda655b2d05069573f9dc8
+  base: './',
+
+
+
   plugins: [
     react(),
     electron([
